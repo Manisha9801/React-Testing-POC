90dccb67477529ef615232b1f01830cb302c5ede
Manisha Kumari <manisha.9801@gmail.com>
2020-06-02 15:30:10 +0530

    adding prop-types in headline as well as testing the props types as well.

--- Utils/index.js
+++ Utils/index.js
@@ -1,4 +1,11 @@
+import checkPropTypes from 'check-prop-types';   
+
 export const findByDataTestAttribute = (component, attr) => {
     const wraper = component.find(`[data-test='${attr}']`);
     return wraper;
+}
+
+export const checkProps = (component, expectedProps) => {
+    const propsErrors = checkPropTypes(component.propTypes,expectedProps,'props',component.name);
+    return propsErrors;
 }
--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './components/header';
 import Headline from './components/headline';
 import './app.scss';
 
+const tempArr = [{
+  fName : 'Joe',
+  lName : 'Bloggs',
+  email : 'manisha.9801@yahoo.com',
+  age : 25,
+  onLineStatus : true
+}]
 
-function App() {
-  return (
-    <div className="App">
-        <Header />
-        <section className="main">
-          <Headline header="POST" desc="Click the button to render Posts" />
-        </section>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <Header />
+          <section className="main">
+            <Headline header="POST" desc="Click the button to render Posts" tempArr = {tempArr} />
+          </section>
+      </div>
+    );
+  }
 }
 
 export default App;
